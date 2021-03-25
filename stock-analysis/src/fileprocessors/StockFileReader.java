491b8e3c1bc7b87a9e06781a1f47e9a683285976
Ralph Wright <ralphwrightengineer@gmail.com>
2021-03-25 15:14:31 -0500

    finishing up project

--- stock-analysis/src/fileprocessors/StockFileReader.java
+++ stock-analysis/src/fileprocessors/StockFileReader.java
@@ -36,6 +36,15 @@ public class StockFileReader {
 	 */
 	public List<String> readFileData() throws IOException{
 		List<String> lines = new ArrayList<String>();
+		try(BufferedReader br = new BufferedReader(new FileReader(filePath))){
+			// skip the first line
+			br.readLine();
+			String line = null;
+			//advance from second line
+			while((line = br.readLine()) != null) {
+				lines.add(line);
+			}
+		}
 	    return lines;
 	}
 	
